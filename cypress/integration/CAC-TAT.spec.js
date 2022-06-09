4885308e5d04004f1a63df88bb31a36d971ff8ff
julianokoslowisk <47350166+julianokoslowisk@users.noreply.github.com>
2022-06-09 12:21:05 -0300

    Exercicio 1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,11 +1,21 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-
-    it('verifica o título da aplicação', function() {
+    beforeEach(function() {
         cy.visit('./src/index.html')
-
-        cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
-  
     })
-  })
+
+    it('verifica o título da aplicação', function() { 
+        cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')  
+    })
+
+    it.only('preenche os campos obrigatórios e envia o formulário', function() {
+        cy.get('#firstName').type('Juliano')
+        cy.get('#lastName').type('Reis')
+        cy.get('#email').type('julianokoslowisk@gmail.com')
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()
+        cy.get('.success').should('be.visible')
+          
+    })
+})
