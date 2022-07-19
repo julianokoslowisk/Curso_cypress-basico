28cb4def1ff21f9e9f2cdce3cfefbf10e5b5a822
julianokoslowisk <julianokoslowisk@gmail.com>
2022-07-19 13:50:08 -0300

    .....

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -127,12 +127,30 @@ describe('Central de Atendimento ao Cliente TAT', function () {
 
   })
 
-  it.only('Marca Ambos chekbox depois desmarca o ultimo ")', function () {
+  it('seleciona um arquivo simulando um drag-and-drop', function () {
     cy.get('input[type="file"] ')
       .should('not.have.value')
-      .selectFile('./cypress/fixtures/example.json')
+      .selectFile('./cypress/fixtures/example.json' ,{ action: 'drag-drop' })
       .should(function ($input) {
         expect($input[0].files[0].name).to.equal('example.json')
       })
+  })
+})
+
+  it.only('seleciona um arquivo utilizando uma fixture para qual foi dada um alias', function () {
+  cy.fixture('example.json').as('sampleFile')
+  cy.get('input[type="file"]')
+    .selectFile('@sampleFile')
+    .should(function ($input) {
+      expect($input[0].files[0].name).to.equal('example.json')
     })
   })
+
+
+
+
+    
+   
+
+
+
